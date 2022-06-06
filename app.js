b1ec390cd084caac48921297877cfac2c2eb5612
Pieter Pretorius <pieterpretorius223@gmail.com>
2022-06-06 22:01:24 +0200

    reuploading

--- app.js
+++ app.js
@@ -1,56 +1,63 @@
-var express 	= require("express"),
-	app 		= express(),
-	bodyParser 	= require("body-parser"),
-	mongoose 	= require("mongoose"),
-	flash		= require("connect-flash"),
-	passport 	= require("passport"),
-	LocalStrategy = require("passport-local"),
-	methodOverride = require("method-override"),
-	Campground 	= require("./models/campground"),
-	Comment 	= require("./models/comment"),
-	User		= require("./models/user"),
-	seedDB 		= require("./seeds"); 
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	flash = require('connect-flash'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local'),
+	methodOverride = require('method-override'),
+	Campground = require('./models/campground'),
+	Comment = require('./models/comment'),
+	User = require('./models/user'),
+	seedDB = require('./seeds');
 
 //requiring routes
-var commentRoutes = require("./routes/comments"),
-	campgroundRoutes = require("./routes/campgrounds"),
-	indexRoutes = require("./routes/index");
+var commentRoutes = require('./routes/comments'),
+	campgroundRoutes = require('./routes/campgrounds'),
+	indexRoutes = require('./routes/index');
 
 var url = process.env.MONGO_URL;
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect(url, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 app.use(flash());
 
 app.locals.moment = require('moment');
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "Nugsy Tugsy is the cutest cat in the world!",
-	resave: false,
-	saveUninitialized: false,
-}));
+app.use(
+	require('express-session')({
+		secret: 'Nugsy Tugsy is the cutest cat in the world!',
+		resave: false,
+		saveUninitialized: false,
+	}),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
 	next();
 });
 
-app.use("/", indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use('/', indexRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
 // LISTEN PORT
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
-	console.log("The YelpCamp Server has Started!");
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+	console.log('The YelpCamp Server has Started!');
 });
